db61efb583247ea886d4c47d11701c05af80d013
iskandar777-dar <54578330+iskandar777-dar@users.noreply.github.com>
2023-01-23 20:04:08 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -58,7 +58,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/iskandar777-dar/Darmimusik",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # GIT TOKEN (jika repo Anda yang diedit bersifat pribadi)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
